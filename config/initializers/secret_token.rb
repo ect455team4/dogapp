0cbbe58d1f951f700de23ba310ba36e66dd9dc59
ect455team4 <ect455team4@gmail.com>
2012-10-21 20:30:21 -0500

    Rails app - PSQL

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dogapp::Application.config.secret_token = '05df74bfecb2de476499bd290b3eef4ff2486b48c0a73b132e578cf9ca5730c8a61e5925f5f7db684b9f0025fb76a040d61254508534fd715a71b9423f894916'
