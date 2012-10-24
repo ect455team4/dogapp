c586031511642ead7095d1df3a93c3cc1a0bd39b
ect455team4 <ect455team4@gmail.com>
2012-10-24 00:00:20 -0500

    created migration add_email_uniqueness_index

---
+++ db/migrate/20121024045144_add_email_uniqueness_index.rb
@@ -0,0 +1,13 @@
+# Migration for enforcing email uniqueness
+# add_index method adds an index on the email column of the users table
+# the index by itself doesnt enforce uniqueness, but the option ':unique => true' does
+
+class AddEmailUniquenessIndex < ActiveRecord::Migration
+  def self.up
+  	add_index :users, :email, unique => true 
+  end
+
+  def self.down
+  	remove_index :users, :email
+  end
+end
