9fa0adbcefd837b78ef9d2d9080807eb755d0213
ect455team4 <ect455team4@gmail.com>
2012-10-28 16:27:54 -0500

    protected pages finished

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-  
+  before_filter  :authenticate, :only => [:edit, :update]
+  before_filter  :correct_user, :only => [:edit, :update]
+
   def new
   	@user = User.new
   	@title = "Join"
@@ -21,4 +23,29 @@ class UsersController < ApplicationController
   		render 'new'
   	end
   end
+
+  def edit
+    @title = "Edit User"
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile Updated"
+      redirect_to @user
+    else
+      @title = "Edit User"
+      render 'edit' 
+    end
+  end
+
+  private
+    def authenticate
+      deny_access unless signed_in?
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
 end
